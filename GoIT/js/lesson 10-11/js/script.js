de31aaf0bad6a6e3860529e57fd43f4f5e8ab3cc
Kolland <koland.my@gmail.com>
2015-12-10 12:49:15 +0200

    del unused else, add cycle for userName enter

--- GoIT/js/lesson 10-11/js/script.js
+++ GoIT/js/lesson 10-11/js/script.js
@@ -2,30 +2,45 @@
 var num  = +prompt('Введите число:');
 var deg  = +prompt('Введите показатель степени:');
 var	result = 1;
+
 function pow(a, b) {
 	for (var i = 0; i < b; i++) {
 		result = result * a;
 	};
 }
+
 pow(num, deg)
+
 console.log(result);
 
 // Проверка пользователя
 var namesArr = [];
 var flag = false;
+
 for (var i = 0; i < 5; i++) {
 	namesArr.push(prompt('Заполните масив имен:'));
 };
-var userName = prompt('Введите свое имя:')
+
+var j = 0;
+
+for (var i = 0; i <= j; i++) {
+	var userName = prompt('Введите свое имя:')
+	if (userName === '') {
+		j++
+	}
+};
+
 for (var i = 0; i < namesArr.length; i++) {
 	if (namesArr[i] === userName) {
 		flag = true;
-	} else{};
+	};
 };
+
 if (flag) {
-	alert(userName + ', вы успешно вошли!')
+	alert( userName + ', вы успешно вошли!' )
 } else{
-	alert('ОШИБКА: Пользователя с таким именем не существует.')
+	alert( 'ОШИБКА: Пользователя с таким именем не существует.' )
 };
+
 console.log(namesArr);
 console.log(userName);
