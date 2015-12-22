176d2de3d7b7c1dfde89de81f8e0727bfd50c7f7
Kolland <koland.my@gmail.com>
2015-12-22 14:54:34 +0200

    JS_3-4 changed if structure for createElement,
    - deleted unused divs
    - fix formating

--- GoIT/js/lesson 3-4/js/script.js
+++ GoIT/js/lesson 3-4/js/script.js
@@ -1,82 +1,89 @@
 var app = {
+
 	createElement: function (tagParams) {
-		var element = document.createElement(tagParams.tagName);
+		var element = document.createElement(tagParams.tag);
 		element.className = tagParams.tagClass;
-		element.innerHTML = tagParams.tagContent;
-		if (tagParams.tagName === 'input') {
+		element.innerHTML = tagParams.content;
+
+		if (tagParams.tag === 'input' || tagParams.tag === 'button') {
 			element.setAttribute('type', tagParams.inputType);
-		}
-		if (tagParams.inputType === 'submit') {
-			element.setAttribute('value', tagParams.inputValue);
-		}
-		tagParams.tagParrent.appendChild(element);
+			
+			if (tagParams.inputType === 'submit' && tagParams.tag === 'input') {
+				element.setAttribute('value', tagParams.inputValue);
+			};
+		};
+
+		tagParams.parrent.appendChild(element);
+
 		return element;
 	},
-	generateQuestions: function(QuestionsAmount, AnswersAmount) {
-		for (var i = 0; i < QuestionsAmount; i++) {
+
+	generateQuestions: function(questionsAmount, answersAmount) {
+		for (var i = 0; i < questionsAmount; i++) {
 			this.createElement ({
-				tagName: 'h3',
+				tag: 'h3',
 				tagClass: 'text-center bg-primary',
-				tagContent: 'Вопрос №' + (i +1),
-				tagParrent: centerDiv
+				content: 'Вопрос №' + (i +1),
+				parrent: centerDiv
 			});
+
 			var ul = this.createElement ({
-					tagName: 'ul',
+					tag: 'ul',
 					tagClass: 'center-block',
-					tagContent: '',
-					tagParrent: centerDiv
+					content: '',
+					parrent: centerDiv
 			})
-			for (var j = 0; j < AnswersAmount; j++) {
+
+			for (var j = 0; j < answersAmount; j++) {
 				var li = this.createElement ({
-					tagName: 'li',
+					tag: 'li',
 					tagClass: 'list-group-item',
-					tagContent: '',
-					tagParrent: ul
+					content: '',
+					parrent: ul
 				})
 				var label = this.createElement ({
-					tagName: 'label',
-					tagContent: 'Вариант ответа №' + (j +1),
-					tagParrent: li
+					tag: 'label',
+					content: 'Вариант ответа №' + (j +1),
+					parrent: li
 				})
 				this.createElement ({
-					tagName: 'input',
+					tag: 'input',
 					tagClass: 'pull-left',
 					inputType: 'checkbox',
-					tagContent: '',
-					tagParrent: label
+					content: '',
+					parrent: label
 				})
 			};
 		};
 	}
-}
-
-var body = document.querySelector('body');
-//generate div bootsrap columns
-for (var i = 0; i < 3; i++) {
-	app.createElement({
-	tagName: 'div',
-	tagClass: 'col-md-4',
-	tagContent: '',
-	tagParrent: body
-	})
 };
 
-var centerDiv = document.getElementsByTagName('div')[1];
+document.body.className = 'container';
+var body = document.body;
+
+//generate div bootsrap columns
+
+var centerDiv = app.createElement({
+	tag: 'div',
+	tagClass: 'col-md-12',
+	content: '',
+	parrent: body
+	});
 
 app.createElement({
-	tagName: 'h2',
+	tag: 'h2',
 	tagClass: 'text-center bg-danger',
-	tagContent: 'Тест по программированию',
-	tagParrent: centerDiv
+	content: 'Тест по программированию',
+	parrent: centerDiv
 	})
 
 app.generateQuestions(3, 3);
 
 app.createElement({
-	tagName: 'input',
+	tag: 'input',
 	inputType: 'submit',
 	tagClass: 'btn btn-lg btn-success center-block',
 	inputValue: 'Проверить мои результаты',
-	tagParrent: centerDiv
+	parrent: centerDiv
 	})
 
